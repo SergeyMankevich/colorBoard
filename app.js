418e241b839ee3e3d1acd85c6a6162decb230e4a
SergeyMankevich <83647450+SergeyMankevich@users.noreply.github.com>
2024-04-02 23:26:36 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     board.append(square)
 }
 
-// function setColor(element) {
-//     const color = getRandomColor()
-//     element.style.backgroundColor = color
-//     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
-// }
-// function removeColor(element) {
-//     element.style.backgroundColor = '#1d1d1d'
-//     element.style.boxShadow = `0 0 2px #000`
-// }
 function setColor(event) {
     const element = event.target
     const color = getRandomColor()
